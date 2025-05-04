db0b28c9f9540da8178032f0af2fd58b295e6e20
Prakash Rai <prakashraipersonal@gmail.com>
2025-05-04 18:14:36 +0530

    Update __init__.py

--- dynamic_watchlist_lib/__init__.py
+++ dynamic_watchlist_lib/__init__.py
@@ -1,21 +1,19 @@
 """
-Dynamic Watchlist Library
+dynamic_watchlist_lib/__init__.py
 
-Exposes core modules for data fetching, metrics calculations, sector mapping, and news/events.
+Package initialization.
 """
-__version__ = "0.1.0"
 
-from .config import KITE_API_KEY, KITE_API_SECRET, KITE_ACCESS_TOKEN, FT_NEWS_API_KEY
+from .config import KITE_API_KEY, KITE_API_SECRET, KITE_ACCESS_TOKEN, IST
+from .utils import cache_ttl, now_ist
 from .data_fetcher import (
     get_kite_client,
     get_instrument_map,
     fetch_intraday_ohlc,
     fetch_daily_ohlc,
     fetch_futures_oi,
-    fetch_latest_news,
-    fetch_corporate_events,
 )
-from .metrics import calculate_vwap, calculate_pivots, detect_volume_surge
 from .sector_mapping import get_sector_index, get_intraday_change, get_sector_deviation
-from .news_events import get_recent_news, get_corporate_actions, get_news_and_events
-from .utils import now_ist, to_ist, cache_ttl
+from .metrics import calculate_vwap, calculate_pivots, detect_volume_surge
+
+__version__ = "0.1.0"
